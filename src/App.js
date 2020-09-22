cc3df54dcd90b5f2c93d661a30b64f3b5a34c44e
unknown <sakshisemalti58@gmail.com>
2020-09-22 21:23:07 +0530

    meme generator

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Component/header'
+import MemeGenerator from './Component/memeGenerator'
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <MemeGenerator />
+    </div>
+
+
+  )
+}
+
+export default App;
